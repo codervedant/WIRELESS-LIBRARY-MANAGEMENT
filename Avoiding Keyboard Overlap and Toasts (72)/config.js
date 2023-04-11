615ab372df3e830d8efc2ae779a41605a2b16a4c
codervedant <74607608+codervedant@users.noreply.github.com>
2023-04-11 20:48:28 +0530

    Add files via upload

---
+++ Avoiding Keyboard Overlap and Toasts (72)/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB4XTvzSJOEccs_ftiXrHWmFh1JdGtmqRk",
+  authDomain: "willy-app-cabd9.firebaseapp.com",
+  projectId: "willy-app-cabd9",
+  storageBucket: "willy-app-cabd9.appspot.com",
+  messagingSenderId: "199827646806",
+  appId: "1:199827646806:web:3d10fe2bc209905cf80ef7"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase.firestore();
---
+++ Capstone Class - Authenticating Users (75)/whitehatjr/wily_authentication/config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB4XTvzSJOEccs_ftiXrHWmFh1JdGtmqRk",
+  authDomain: "willy-app-cabd9.firebaseapp.com",
+  projectId: "willy-app-cabd9",
+  databaseURL: "https://willy-app-cabd9-default-rtdb.firebaseio.com",
+  storageBucket: "willy-app-cabd9.appspot.com",
+  messagingSenderId: "199827646806",
+  appId: "1:199827646806:web:3d10fe2bc209905cf80ef7"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase.firestore();
